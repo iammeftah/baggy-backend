aae141d5926738b58b52b022065fbf2a19b738b4
Meftah Ahmed Reda <meftahahmedreda02@gmail.com>
2026-01-12 16:11:28 +0100

    clear cache

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// In routes/web.php or api.php
+Route::get('/clear-cache', function() {
+    \Artisan::call('config:clear');
+    \Artisan::call('cache:clear');
+    \Artisan::call('view:clear');
+    \Artisan::call('route:clear');
+
+    return 'Cache cleared!';
+});
